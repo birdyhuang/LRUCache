dd5f400643124b9cf9ce334d4d69591786240127
Peng <peng_huang007@hotmail.com>
2016-05-18 16:06:02 -0400

    1. Make the inner class Entry more decoupled by making all properties private. 2. Provide more test cases. 3. Guarantee high read frequency and low write frequency by using ReadWrtiteLock.

--- LruCache.java
+++ LruCache.java
@@ -3,22 +3,48 @@
  */
 
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class LruCache<K, V> {
     public static class Entry<K, V> {
-        K key;
-        V value;
-        Entry<K, V> prev, next;
+        private K key;
+        private V value;
+        private Entry<K, V> prev, next;
         public Entry(K k, V v) {
             key = k;
             value = v;
             prev = null;
             next = null;
         }
+        public void setKey(K key) {
+            this.key = key;
+        }
+        public void setValue(V value) {
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
+        public void setPrev(Entry<K, V> prev) {
+            this.prev = prev;
+        }
+        public void setNext(Entry<K, V> next) {
+            this.next = next;
+        }
+        public Entry<K, V> getPrev() {
+            return prev;
+        }
+        public Entry<K, V> getNext() {
+            return next;
+        }
     }
     private Entry<K, V> head, tail;
     private Map<K, Entry<K, V>> map;
     private int capacity, size;
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     private static final int DEFAULT_SIZE = 10;
 
     public LruCache () {
@@ -26,94 +52,123 @@ public class LruCache<K, V> {
         size = 0;
         head = new Entry<K, V>(null, null);
         tail = new Entry<K, V>(null, null);
-        head.next = tail;
-        tail.next = head;
+        head.setNext(tail);
+        tail.setPrev(head);
         map = new HashMap<K, Entry<K, V>>();
     }
 
     public LruCache (int capacity) {
+        if (capacity <= 0) {
+            System.out.println("Capacity must be larger than 0");
+            System.exit(1);
+        }
         this.capacity = capacity;
         size = 0;
         head = new Entry<K, V>(null, null);
         tail = new Entry<K, V>(null, null);
-        head.next = tail;
-        tail.prev = head;
+        head.setNext(tail);
+        tail.setPrev(head);
         map = new HashMap<K, Entry<K, V>>();
     }
 
-    public static <K, V> void printCache(LruCache<K, V> cache) {
-        Entry<K, V> it = cache.head.next;
-        while (it != cache.tail) {
-            System.out.println(it.value);
-            it = it.next;
+    public void printCache() {
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            Entry<K, V> it = head.getNext();
+            while (it != tail) {
+                System.out.println(it.value);
+                it = it.getNext();
+            }
+        } finally {
+            readLock.unlock();
         }
     }
 
-    public static <K, V> String getByString(LruCache<K, V> cache) {
-        Entry<K, V> it = cache.head.next;
-        StringBuilder sb = new StringBuilder();
-        while (it != cache.tail) {
-            sb.append(it.value + "; ");
-            it = it.next;
+    public  String getByString() {
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            Entry<K, V> it = head.getNext();
+            StringBuilder sb = new StringBuilder();
+            while (it != tail) {
+                sb.append(it.value + " ");
+                it = it.getNext();
+            }
+            return sb.toString();
+        } finally {
+            readLock.unlock();
         }
-        return sb.toString();
     }
 
     public V get(K key) {
-        Entry<K, V> entry = map.get(key);
-        if (entry != null) {
-            moveToTail(entry);
-            return entry.value;
-        } else {
-            return null;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            Entry<K, V> entry = map.get(key);
+            if (entry != null) {
+                moveToTail(entry);
+                return entry.getValue();
+            } else {
+                return null;
+            }
+        } finally {
+            readLock.unlock();
         }
     }
 
     public void set(K key, V value) {
-        Entry<K, V> entry = map.get(key);
-        if (entry == null) {
-            entry = new Entry<K, V>(key, value);
-            size++;
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            Entry<K, V> entry = map.get(key);
+            if (entry == null) {
+                entry = new Entry<K, V>(key, value);
+                size++;
+            }
+            entry.setValue(value);
+            moveToTail(entry);
+            map.put(key, entry);
+            checkOverFlow();
+        } finally {
+            writeLock.unlock();
         }
-        moveToTail(entry);
-        map.put(key, entry);
-        checkOverFlow();
     }
 
     public void remove(K key) {
-        Entry<K, V> entry = map.get(key);
-        if (entry == null) { return; }
-        entry.prev.next = entry.next;
-        entry.next.prev = entry.prev;
-        map.remove(key);
-        size--;
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            Entry<K, V> entry = map.get(key);
+            if (entry == null) {
+                throw new NoSuchElementException();
+            }
+            entry.getPrev().setNext(entry.getNext());
+            entry.getNext().setPrev(entry.getPrev());
+            map.remove(key);
+            size--;
+        } catch (NoSuchElementException e) {
+            System.err.println("No such element");
+            // do something
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     private void checkOverFlow() {
         if (size > capacity) {
-            remove(head.next.key);
+            remove(head.getNext().getKey());
         }
     }
 
     private void moveToTail(Entry<K, V> entry) {
         if (entry.next != null) {
-            entry.prev.next = entry.next;
-            entry.next.prev = entry.prev;
+            entry.getPrev().setNext(entry.getNext());
+            entry.getNext().setPrev(entry.getPrev());
         }
-        tail.prev.next = entry;
-        entry.prev = tail.prev;
-        entry.next = tail;
-        tail.prev = entry;
-    }
-
-//    /* Used to add a new entry */
-//    private void appendToTail(Entry<K, V> entry) {
-//        tail.prev.next = entry;
-//        entry.prev = tail.prev;
-//        entry.next = tail;
-//        tail.prev = entry;
-//    }
-
-    public static void main(String[] args) {
+        tail.getPrev().setNext(entry);
+        entry.setPrev(tail.getPrev());
+        entry.setNext(tail);
+        tail.setPrev(entry);
     }
 }
--- TestLruCache.java
+++ TestLruCache.java
@@ -5,7 +5,6 @@
 import junit.framework.*;
 import org.junit.Test;
 import org.junit.After;
-//import static org.junit.Assert.*;
 
 public class TestLruCache extends TestCase {
     public Object dInput;
@@ -18,15 +17,53 @@ public class TestLruCache extends TestCase {
     }
 
     @Test
-    public void testGeneral() {
-        cache = new LruCache<Integer, String>(15);
+    public void testSizeControl() {
+        cache = new LruCache<Integer, Integer>(15);
         for (int i = 1; i <= 20; i++) {
             cache.set(i, i);
         }
         System.out.println();
-//        LruCache.printCache(cache);
-        String result = LruCache.getByString(cache);
-        dOutput = "6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20; ";
+        String result = cache.getByString();
+        dOutput = "6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ";
+        assertEquals(result, dOutput);
+    }
+
+    @Test
+    public void testRead() {
+        cache = new LruCache<Integer, Integer>(10);
+        for (int i = 1; i <= 10; i++) {
+            cache.set(i, i);
+        }
+        for (int i = 10; i >= 1; i--) {
+            cache.get(i);
+        }
+        String result = cache.getByString();
+        dOutput = "10 9 8 7 6 5 4 3 2 1 ";
+        assertEquals(result, dOutput);
+    }
+
+    @Test
+    public void testRemoveNotExist() {
+        cache = new LruCache<Integer, Integer>();
+        cache.remove(1);
+    }
+
+    @Test
+    public void testReadNotExist() {
+        cache = new LruCache<Integer, Integer>();
+        Integer result = (Integer) cache.get(1);
+        dOutput = null;
+        assertEquals(result, dOutput);
+    }
+
+    @Test
+    public void testGeneral() {
+        cache = new LruCache<Integer, Integer>();
+        cache.set(2, 1);
+        cache.set(2, 2);
+        cache.set(1, 2);
+        String result = cache.getByString();
+        dOutput = "2 2 ";
         assertEquals(result, dOutput);
     }
 }
